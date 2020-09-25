235dfa38e2bcbc6649cffdb521138c12ede3bc46
Divyanshi Agrawal <divsagr3@gmail.com>
2020-09-25 18:19:30 +0530

    done

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8002",
+  baseURL: "https://tinder1-backend.herokuapp.com",
 });
 
 export default instance;
